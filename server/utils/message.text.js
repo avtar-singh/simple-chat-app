26bcd431ab73a65b25f89cbbc0de1fbb747ecb2b
Avtar Singh <hellsider7@gmail.com>
2023-02-21 13:26:19 +0530

    Added Test

---
+++ server/utils/message.text.js
@@ -0,0 +1,16 @@
+const mocha = require('mocha');
+const chai = require('chai');
+const expect = chai.expect;
+
+const {generateMessage} = require('./message');
+
+describe('generateMessage'), () => {
+    it('should generate correct message object'), () => {
+        const from = 'Jane';
+        const text = 'Hey!Whats up!';
+        const message = generateMessage(from, text);
+        expect(message.from).to.equal(from);
+        expect(message.text).to.equal(text);
+        expect(message.createdAt).to.be.a('number');
+    };
+};
